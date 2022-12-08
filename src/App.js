fdb2d9ae03e3e69fa2a2e043e25184652f3cd53e
jia-ayo <milkjia11@gmail.com>
2022-12-08 04:51:24 +0100

    connect button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'jia_ayo';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null)
   const checkIfWalletIsConnected = async()=>{
     try{
       const {solana} = window;
-      if {solana} {
-        if (solana.isPhatom){
+      if (solana) {
+        if (solana.isPhantom){
           console.log("Phantom wallet found")
+
+          const response = await solana.connect({onlyIfTrusted: true})
+          console.log(`connected with e wallet of publickey: ${response.publicKey.toString()}`)
+          setWalletAddress(response.publicKey.toString())
         }
       }else{
         alert("solana object not found! get a phantom wallet")
@@ -21,19 +26,39 @@ const App = () => {
       console.error(error)
     }
   }
-  useEffect(()=>{
-    const onload = asyn()=>{
+
+  const connectWallet = async ()=>{
+    const {solana} = window;
+    if(solana){
+      const response = solana.connect();
+      console.log(`connected with e wallet of publickey: ${response.publicKey.toString()}`)
+      setWalletAddress(response.publicKey.toString())
+    }
+  };
+
+  const renderNotConnectedContainer = ()=>
+   ( <button
+    className='cta-button connect-wallet-button'
+    onClick={connectWallet}
+    >
+      connect wallet
+    </button>)
+    useEffect(()=>{
+    const onLoad = async()=>{
       await checkIfWalletIsConnected()
     }
-  })
+    window.addEventListener("load", onLoad)
+    return () => window.remove("load", onLoad)
+  },[])
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress? "authed-container" : "container"}>
         <div className="header-container">
           <p className="header">🖼 GIF Portal</p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
